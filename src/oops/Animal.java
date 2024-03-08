658013c3c83729b4b1a70beb2b5991595bea74a2
pravin-singh-parihar <parihar.pravinsingh772005@gmail.com>
2024-03-08 19:44:03 +0530

    first commit

---
+++ src/oops/Animal.java
@@ -0,0 +1,13 @@
+package oops;
+
+public class Animal {
+    public String ranveer(){
+        System.out.println("papa papa papa ");
+        return "papa papa papa";
+    }
+    public String rashmika(){
+        System.out.println("-------------------------------------");
+
+        return"----------------------------";
+    }
+}
