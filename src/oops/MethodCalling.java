9d0a60333bc56ee80b731a07b7530d2fd142778c
pravin-singh-parihar <parihar.pravinsingh772005@gmail.com>
2024-03-08 19:53:09 +0530

    first commit

--- src/oops/MethodCalling.java
+++ src/oops/MethodCalling.java
@@ -3,5 +3,8 @@ package oops;
 public class MethodCalling {
     public static void main(String[] args) {
 
+     Animal animal=new Animal();
+     animal.ranveer();
+     animal.rashmika();
     }
 }
