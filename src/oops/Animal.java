bdab4262733e596d718ef97b301e2159fbbcc7ac
pravin-singh-parihar <parihar.pravinsingh772005@gmail.com>
2024-03-08 20:04:38 +0530

    first commmit

--- src/oops/Animal.java
+++ src/oops/Animal.java
@@ -1,6 +1,8 @@
 package oops;
 
 public class Animal {
+    // in which a String is the return type whenever we are the use the return than we always add the return
+    //void is the return type we did not add the ruturn in void
     public String ranveer(){
         System.out.println("papa papa papa ");
         return "papa papa papa";
--- src/oops/MethodCalling.java
+++ src/oops/MethodCalling.java
@@ -2,6 +2,7 @@ package oops;
 
 public class MethodCalling {
     public static void main(String[] args) {
+        //ranveer and reshmika is 2 methed that are calling using the creating the object without object we are not able to call the methosd
 
      Animal animal=new Animal();
      animal.ranveer();
